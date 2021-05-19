c3d0f5c378a1a27a44ec535c474004f67005b5c7
ppotatoG <thgus2270@gmail.com>
2021-05-19 11:31:41 +0900

    기본 슬라이드

---
+++ js/event.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.bxslider').bxSlider({
+      slideWidth: 600,
+      pager : false,
+      maxSlides : 8,
+      minSlides : 4,
+      moveSlides : 1,
+      slideMargin : 10
+    });
+  });
