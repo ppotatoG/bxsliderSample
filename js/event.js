7407366843504ac6a469ca9f361a724712415c80
ppotatoG <thgus2270@gmail.com>
2021-05-19 13:47:23 +0900

    slide tab contents 생성

--- js/event.js
+++ js/event.js
@@ -2,9 +2,10 @@ $(function(){
     $('.bxslider').bxSlider({
       slideWidth: 600,
       pager : false,
-      maxSlides : 8,
+      maxSlides :4,
       minSlides : 4,
       moveSlides : 1,
       slideMargin : 10
     });
-  });
+});
+
